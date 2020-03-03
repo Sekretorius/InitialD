f41b7b49a47208af136850b58699549775c2a18e
Sekretorius <54936405+Sekretorius@users.noreply.github.com>
2020-03-03 23:48:14 +0200

    Interactions w/ objects | link: https://initiald.atlassian.net/browse/IND-12?atlOrigin=eyJpIjoiOWE2NzQ3NGZkMTUxNDE2YThlN2I3OTA4ZTg1M2VlNDYiLCJwIjoiaiJ9
    Player can interact with objects and create different types of interactions

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+    public bool isInteractable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isInteractable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeColor();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            isInteractable = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            isInteractable = false;
+    }
+
+    private void ChangeColor()
+    {
+        if (isInteractable && Input.GetKey(KeyCode.E))
+            gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+    }
+}
---
+++ Assets/Scripts/ShowUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUI : MonoBehaviour
+{
+
+    public GameObject UI_object;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI_object.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            UI_object.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            UI_object.SetActive(false);
+    }
+
+}
