c6f598c3bd94d566ae3d46c97fb349bc91a39d0d
Sekretorius <54936405+Sekretorius@users.noreply.github.com>
2020-03-04 00:29:09 +0200

    Couple of assets

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Interactable : MonoBehaviour
+public class Interactable : MonoBehaviour
 {
 
     public bool isInteractable;
