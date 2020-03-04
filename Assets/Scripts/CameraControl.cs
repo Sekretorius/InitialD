8f97c4dcc29dacfe1e8590abcfffebe91f15b98a
Mantvydas <mantvydasri99@gmail.com>
2020-03-04 09:30:30 +0200

    https://initiald.atlassian.net/browse/IND-13?atlOrigin=eyJpIjoiNjI2ZDM3N2EyMTIzNGIxZWIyNjlhNDgyZDgwNDI4NjMiLCJwIjoiaiJ9
    Made basic camera movement, adjusted positions and minor tweaked player physics

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public int sensitivity = 10;
+    public GameObject player;
+    // Start is called before the first frame update
+    Vector3 stockPosition;
+    float width;
+    float height;
+
+    void Start()
+    {
+         width = Screen.width;
+         height = Screen.height;
+         stockPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position+stockPosition + 
+            new Vector3(Input.mousePosition.x / width * sensitivity, Input.mousePosition.y / height * sensitivity, 0);
+    }
+}
