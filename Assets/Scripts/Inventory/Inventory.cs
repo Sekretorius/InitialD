483289deab3ac06aa783213868d2d109a9b104e7
Mantvydas <mantvydasri99@gmail.com>
2020-03-26 22:51:26 +0200

    Inventory implementation + sprites | https://initiald.atlassian.net/browse/IND-19
    Added sprites, implemented inventory

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] List<Item> items;
+    //  [SerializeField] Transform itemsParent;
+    [SerializeField] ItemSlot[] itemSlots;
+    private void OnValidate()
     {
-        
+        //  if (itemsParent != null)
+        itemSlots = gameObject.GetComponentsInChildren<ItemSlot>();
+        RefreshUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    void RefreshUI()
     {
-        
+        int i = 0;
+        for (; i < items.Count && i < itemSlots.Length; i++)
+        {
+            itemSlots[i].Item = items[i];
+        }
+
+        for (; i < itemSlots.Length; i++)
+        {
+            itemSlots[i].Item = null;
+        }
     }
 }
---
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ItemSlot : MonoBehaviour
+{
+    [SerializeField] Image Image;
+    public Item _item;
+    public Item Item
+    {
+        get { return _item; }
+        set
+        {
+            _item = value;
+            if(_item == null)
+            {
+                Image.enabled = false;
+            }
+            else
+            {
+                Image.sprite = _item.Icon;
+                Image.enabled = true;
+            }
+        }
+    }
+    private void OnValidate()
+    {
+        if (Image == null)
+        {
+            Image = GetComponent<Image>();
+            //Debug.Log("aaaaaaaaaaaaaa");
+        }
+
+    }
+}
