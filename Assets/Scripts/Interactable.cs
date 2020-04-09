0dabcfa422dbdadc930406d905fb251d87911196
LTitro <61280874+LTitro@users.noreply.github.com>
2020-04-09 12:31:46 +0300

    https://initiald.atlassian.net/browse/IND-33
    bug fixes

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -12,7 +12,7 @@ public class Interactable : MonoBehaviour
         interactingObject = null;
         IsInteractable = false;
     }
-    private void Update()
+    private void FixedUpdate()
     {
         OnEvent();
     }
