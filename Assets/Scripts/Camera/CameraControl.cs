ba9edf577b4effc001d7717d62d4fcb3d0c40bdf
Mantvydas <mantvydasri99@gmail.com>
2020-04-09 14:22:23 +0300

    https://initiald.atlassian.net/browse/IND-36

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public int sensitivity = 10;
-    public int zoomFOV;
+    public int zoomSize;
     public GameObject player;
     public Vector3 mousePosition { get; private set; }
     // Start is called before the first frame update
     Vector3 stockPosition;
     float width;
     float height;
-    float stockFOV;
+    float stockSize;
     public float zoomSpeed;
     //public float fovDiff;
     void Start()
@@ -20,8 +20,8 @@ public class CameraControl : MonoBehaviour
         width = Screen.width;
         height = Screen.height;
         stockPosition = transform.position;
-        stockFOV = Camera.main.fieldOfView;
-        // fovDiff = (zoomFOV - stockFOV) / 10;
+        stockSize = Camera.main.orthographicSize;
+        // fovDiff = (zoomSize - stockSize) / 10;
 
     }
 
@@ -32,12 +32,12 @@ public class CameraControl : MonoBehaviour
         transform.position = player.transform.position + stockPosition +
             mousePosition;
 
-        if (Input.GetButton("Fire1") && Camera.main.fieldOfView != zoomFOV)
+        if (Input.GetButton("Fire1") && Camera.main.orthographicSize != zoomSize)
         {
             ZoomIn();
         }
 
-        else if (!Input.GetButton("Fire1") && Camera.main.fieldOfView != stockFOV)
+        else if (!Input.GetButton("Fire1") && Camera.main.orthographicSize != stockSize)
         {
             ZoomOut();
         }
@@ -45,22 +45,22 @@ public class CameraControl : MonoBehaviour
 
     void ZoomIn()
     {
-        if ((Camera.main.fieldOfView + (zoomSpeed * Time.deltaTime)) < zoomFOV)
+        if ((Camera.main.orthographicSize + (zoomSpeed * Time.deltaTime)) < zoomSize)
         {
-            Camera.main.fieldOfView += ((zoomSpeed * Time.deltaTime));
+            Camera.main.orthographicSize += ((zoomSpeed * Time.deltaTime));
             //Debug.Log(Camera.main.fieldOfView);          
         }
-        else Camera.main.fieldOfView = zoomFOV;
+        else Camera.main.orthographicSize = zoomSize;
     }
 
     void ZoomOut()
     {
 
-        if ((Camera.main.fieldOfView - (zoomSpeed * Time.deltaTime)) > stockFOV)
+        if ((Camera.main.orthographicSize - (zoomSpeed * Time.deltaTime)) > stockSize)
         {
-            Camera.main.fieldOfView -= ((zoomSpeed * Time.deltaTime));
+            Camera.main.orthographicSize -= ((zoomSpeed * Time.deltaTime));
             // Debug.Log(Camera.main.fieldOfView);
         }
-        else Camera.main.fieldOfView = stockFOV;
+        else Camera.main.orthographicSize = stockSize;
     }
 }
