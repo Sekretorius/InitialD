2c5311dcb826f15cc7a2acbfa3148be97dff79c0
Sekretorius <54936405+Sekretorius@users.noreply.github.com>
2020-04-19 23:56:23 +0300

    Vending machine script: https://initiald.atlassian.net/browse/IND-58
    Player can now use a vending machines for healing and removes a certain amount of money from the player

--- Assets/Scripts/PlayerScripts/MoneySystem.cs
+++ Assets/Scripts/PlayerScripts/MoneySystem.cs
@@ -6,28 +6,59 @@ using UnityEngine.UI;
 public class MoneySystem : MonoBehaviour
 {
     public int Cash;
+    public int MinLimit;
     public float FadeSpeed;
     public Text CashAdd;
     public Text CashText;
+
+    private Color green; 
+    private Color red;
+    private bool active;
     
 
     void Start()
     {
+        ColorUtility.TryParseHtmlString("#238C20", out green);
+        ColorUtility.TryParseHtmlString("#9D292F", out red);
+
+        if (Cash >= 0)
+            CashText.color = green;
+        else
+            CashText.color = red;
         CashText.GetComponent<Text>().text = Cash + " $";
         CashAdd.color = new Color(CashAdd.color.r, CashAdd.color.g, CashAdd.color.b, 0);
+        active = false;
     }
 
     public void Add(int amount)
     {
+        if(active == true)
+        {
+            StopAllCoroutines();
+            Debug.Log("Worked");
+        }
+        if(amount > 0)
+            CashAdd.GetComponent<Text>().text ="+ " + amount;
+        else
+            CashAdd.GetComponent<Text>().text = amount.ToString();
         UpdateMoney(amount);
-        CashAdd.GetComponent<Text>().text ="+ " + amount;
-
+        active = true;
     }
 
     private void UpdateMoney(int amount)
     {
+        if (amount > 0)
+        {
+            CashAdd.color = new Color(green.r, green.g, green.b, 0);
+            StartCoroutine(CoolCashAnimationAddition(amount, CashText));
+        }
+        else
+        {
+            CashAdd.color = new Color(red.r, red.g, red.b, 0);
+            StartCoroutine(CoolCashAnimationSubtraction(amount, CashText));
+        }
         StartCoroutine(FadeTextToZeroAlpha(FadeSpeed, CashAdd));
-        StartCoroutine(CoolCashAnimation(amount, CashText));       
+        Cash += amount;
     }
 
     public IEnumerator FadeTextToZeroAlpha(float t, Text i)
@@ -40,9 +71,10 @@ public class MoneySystem : MonoBehaviour
         }
     }
 
-    public IEnumerator CoolCashAnimation(int amount, Text i)
+    public IEnumerator CoolCashAnimationAddition(int amount, Text i)
     {
         int temp = Cash;
+        int CashTemp = Cash;
         float time = 5;
        
         float x = 1;    
@@ -51,13 +83,37 @@ public class MoneySystem : MonoBehaviour
         x = time / x;
 
         int count = 0;
-        while (temp <= Cash + amount)
+        while (temp <= CashTemp + amount)
         {
+            i.color = temp >= 0 ? green : red;
             CashText.GetComponent<Text>().text = temp + " $";
-            temp += 1;
+            temp++;
             yield return new WaitForSeconds(count++ * x);
         }
-        Cash += amount;
+        active = false;
+    }
+
+    public IEnumerator CoolCashAnimationSubtraction(int amount, Text i)
+    {
+        amount = -1 * amount;
+        int temp = Cash;
+        int CashTemp = Cash;
+        float time = 5;
+
+        float x = 1;
+        for (int t = 2; t <= amount; t++)
+            x += t;
+        x = time / x;
+
+        int count = 0;
+        while (temp >= CashTemp - amount)
+        {
+            i.color = temp >= 0 ? green : red;
+            CashText.GetComponent<Text>().text = temp + " $";
+            temp--;
+            yield return new WaitForSeconds(count++ * x);
+        }
+        active = false;
     }
 
 }
---
+++ Assets/Scripts/PlayerScripts/Vendingable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vendingable : Interactable
+{
+    public int cost;
+    public int uses;
+
+    private MoneySystem System;
+    private Text text;
+
+
+    private void OnValidate()
+    {
+        System = FindObjectOfType<MoneySystem>();
+        text = GetComponentInChildren<Canvas>().GetComponentInChildren<Text>();
+    }
+
+    protected override void OnEvent()
+    {
+        if (IsInteractable && Input.GetKeyDown(KeyCode.E) && uses > 0)
+        {
+            uses--;
+            System.Add(cost);
+            // Health increase logic
+            if (uses > 0)
+                text.text = "PRESS E TO USE";
+            else
+                text.text = "OUT OF ORDER";
+        }
+    }
+
+    public void Refesh(int count)
+    {
+        uses = count;
+    }
+
+}
