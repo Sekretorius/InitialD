365788da892c6e14a1787a44d5df6577ceb5931c
Mantvydas <mantvydasri99@gmail.com>
2020-06-03 16:31:26 +0300

    Play sound on teleport

--- Assets/Scripts/KillerBean.cs
+++ Assets/Scripts/KillerBean.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class KillerBean : MonoBehaviour
 {
     public GameObject chalice;
+    [SerializeField] AudioSource source;
+    bool triggeredMusic=false;
+    public AudioClip Sound;
+
+    private void Start()
+    {
+        chalice = GameObject.Find("ConsumeTheChalice");
+        source = GameObject.Find("Music").GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -13,6 +22,13 @@ public class KillerBean : MonoBehaviour
         {
             GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
             this.transform.position = new Vector3(this.transform.position.x+0.01f, this.transform.position.y,this.transform.position.z);
+            if(!triggeredMusic)
+            {
+                source.Stop();
+                source.clip = Sound;
+                source.Play();
+                triggeredMusic = true;
+            }
         }     
     }
 }
---
+++ Assets/TriggerSound.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSound : MonoBehaviour
+{
+    public bool Music;
+    [Space]
+    [SerializeField] AudioSource source;
+    public AudioClip Sound;
+
+    void Start()
+    {
+        if(Music)
+            source = GameObject.Find("Music").GetComponent<AudioSource>();
+        else
+                    source = GameObject.Find("Sound").GetComponent<AudioSource>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (source.clip != Sound)
+            source.Stop();
+        if (!source.isPlaying)
+        {
+            source.clip=Sound;
+            source.Play();
+        }
+        
+       
+    }
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
