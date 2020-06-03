999365f4c3e6114ea4b8b7dea9248e3ab4fd7eaa
Mantvydas <mantvydasri99@gmail.com>
2020-06-03 21:44:28 +0300

    Footstep basics?

---
+++ Assets/Scripts/Feet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feet : MonoBehaviour
+{
+    AudioClip[] Footstep;
+    AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GameObject.Find("Sound").GetComponent<AudioSource>();
+        for(int i=0; i<8; i++)
+        {
+            Footstep[i] = (AudioClip)Resources.Load("Footsteps/" + i);
+        }
+    }
+
+    public void PlayFoot()
+    {
+       source.PlayOneShot(Footstep[Random.Range(0, 7)]);
+    }
+
+}
