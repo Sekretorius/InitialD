a213717b823a9342487fed24ea11eaa15eb7b2df
Sekretorius <54936405+Sekretorius@users.noreply.github.com>
2020-06-04 12:57:34 +0300

    End screen

---
+++ Assets/Scripts/MusicTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTrigger : MonoBehaviour
+{
+    public bool Music;
+    public bool Loop;
+    [Space]
+    [SerializeField] AudioSource source;
+    public AudioClip Sound;
+
+    void Start()
+    {
+        if (Music)
+            source = GameObject.Find("Music").GetComponent<AudioSource>();
+        else
+            source = GameObject.Find("Sound").GetComponent<AudioSource>();
+
+        if (source.clip != Sound)
+            source.Stop();
+        if (!source.isPlaying)
+        {
+            source.clip = Sound;
+            //source.loop = Loop;
+            source.Play();         
+        }
+    }
+
+    private void Update()
+    {
+        if (!source.isPlaying)
+        {
+            Application.Quit();
+        }
+    }
+}
